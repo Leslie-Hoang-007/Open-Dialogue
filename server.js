9749de23cd4bd252d7dce5d74429a70a0443a1bc
Leslie-Hoang-007 <111301770+Leslie-Hoang-007@users.noreply.github.com>
2023-12-24 13:52:50 -0500

    added logic for logging in with unique name

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var app = require('express')();
+const express = require('express');  // Add this line
+const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const { v4: uuidv4 } = require('uuid');
+const bodyParser = require('body-parser');  // Add this line
+
+// middleware
+app.use(express.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+var postList=[];
+var nameList=[];
 
 // Handle connections
 io.on('connection', function (socket) {
@@ -27,6 +37,39 @@ io.on('connection', function (socket) {
     console.log('User disconnected');
   });
 });
+
+
+app.post('/submitName', function (req, res) {
+  console.log(req.body);
+  const {name} = req.body;
+
+  // Log the received name
+  console.log("SERVER: Name submitted:", name);
+
+  // Generate UUID for the name
+  const user_id = uuidv4();
+
+  count = 1;
+
+  const submittedName = submitName(name,user_id,count);
+  console.log("RECIEVEDNAME:",submittedName);
+  // Send a response to the client
+  res.json({ message: 'Name submitted successfully' ,userData: submittedName});
+});
+
+
+function submitName(name,user_id,count){
+  const exists = nameList.find((i) => i.name === name);
+  const newName = name+count;
+  console.log(newName);
+  if (!exists){
+    nameList.push({ name, user_id });
+    console.log("NameListAdded:",nameList);
+    return { name, user_id, count };
+  } else{
+    return submitName(newName,user_id,count+1);
+  }
+}
 
 // Start the server
 const PORT = process.env.PORT || 3333;
